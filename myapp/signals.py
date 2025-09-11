77cd03826f23012dade769eadd70d6b31a6e8ac8
Jaehyuk Han <jh.jaehyuk213@gmail.com>
2025-09-11 17:04:51 +0900

    add: [한재혁] django signals를 이용해서 LinkCollectionViewModel과 LinkCollectionLike가 생성 및 삭제 시 조회수와 좋아요 수를 수정할 수 있도록 수정

--- myapp/signals.py
+++ myapp/signals.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
+from django.db.models import F
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
-from myapp.models import Bookmark, UserAvatar, LinkCollection, LinkCollectionThumbnail
+from myapp.models import Bookmark, UserAvatar, LinkCollection, LinkCollectionThumbnail, LinkCollectionLike, \
+    LinkCollectionViewModel
 
 
 @receiver(post_save, sender=User)
@@ -19,3 +21,20 @@ def create_user_avatar(sender, instance, created, **kwargs):
 def create_link_collection_thumbnail(sender, instance, created, **kwargs):
     if created:
         LinkCollectionThumbnail.objects.create(collection=instance)
+
+@receiver(post_save, sender=LinkCollectionLike)
+def increment_like_count(sender, instance, created, **kwargs):
+    if created:
+        instance.collection.likes_count = F('likes_count') + 1
+        instance.collection.save(update_fields=['likes_count'])
+
+@receiver(post_delete, sender=LinkCollectionLike)
+def decrement_like_count(sender, instance, **kwargs):
+    instance.collection.likes_count = F('likes_count') - 1
+    instance.collection.save(update_fields=['likes_count'])
+
+@receiver(post_save, sender=LinkCollectionViewModel)
+def increment_view_count(sender, instance, created, **kwargs):
+    if created:
+        instance.collection.views_count = F('views_count') + 1
+        instance.collection.save(update_fields=['views_count'])
